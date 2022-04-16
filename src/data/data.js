b84cebf6d6f4be62d9051808875ebe68e3f151e3
alessandrothedev <spiridigliozzi.alessandro@gmail.com>
2022-04-16 14:42:24 +0100

    added json data file

---
+++ src/data/data.js
@@ -0,0 +1,41 @@
+const data = [
+    {
+        id: 1,
+        userName: 'Jason Veum',
+        email: 'jasonveum@email.com',
+        email_type: 'Work',
+        importance: 'high',
+        date: 'Mar 12 2022 10:00:00 AM',
+        email_subject: 'Can you take a look at the recent project proposal?',
+        email_body1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        email_body2: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        email_body3: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        email_signature: 'Kind Regards, Jason'
+    },
+    {
+        id: 2,
+        userName: 'AirBnB',
+        email: 'bookings@airbnb.com',
+        email_type: 'Personal',
+        importance: 'medium',
+        date: 'Apr 16 2022 16:00:00 PM',
+        email_subject: 'Your booking has been confirmed!',
+        email_body1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        email_body2: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        email_body3: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        email_signature: 'Kind Regards, AirBnB Team'
+    },
+    {
+        id: 1,
+        userName: 'Molly Smith',
+        email: 'molly-smith@email.com',
+        email_type: 'Personal',
+        importance: 'low',
+        date: 'Jun 09 2021 11:00:00 AM',
+        email_subject: 'Party Invitation',
+        email_body1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        email_body2: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        email_body3: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        email_signature: 'Kind Regards, Jason'
+    }
+]
